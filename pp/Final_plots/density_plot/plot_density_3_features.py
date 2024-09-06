7f84b1bfff6a729f71b19a8e4c957119140cf1b3
ndng1410 <ndng1410@gmail.com>
2024-09-06 22:26:22 +0700

    sửa lại density plot

--- pp/Final_plots/density_plot/plot_density_3_features.py
+++ pp/Final_plots/density_plot/plot_density_3_features.py
@@ -4,6 +4,7 @@ import seaborn as sns
 from time import time
 import numpy as np
 import re, ast
+from scipy import stats
 
 # Load the dataset
 combine_data = pd.read_csv('sampled_students_data_new.csv')
@@ -86,28 +87,52 @@ df = merged_df[['Dropout Status',
                 'passed_percent']]
 
 fig, axs = plt.subplots(1, 3, figsize=(30, 10))
-sns.histplot(data=df, x="average_score", hue="Dropout Status", kde=True, ax=axs[0])
-axs[0].set_title("Density Plot Average Score", fontsize=20)
+sns.histplot(data=df, x="average_score", hue="Dropout Status", kde=True, ax=axs[0], stat='percent', common_norm=False)
+mean_avg = df['average_score'].mean()
+median_avg = df['average_score'].median()
+mode_avg = df['average_score'].mode()[0]
+
+axs[0].set_title("Distribution of Average Score", fontsize=20)
 axs[0].set_xlabel("Average Score", fontsize=20)
-axs[0].set_ylabel("Density", fontsize=20)
+axs[0].set_ylabel("", fontsize=20)
 axs[0].tick_params(axis='x', labelsize=15)
 axs[0].tick_params(axis='y', labelsize=15)
+axs[0].yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{x / 100:.2f}'))
+axs[0].axvline(mean_avg, color='red', linestyle='--', label=f'Mean: {mean_avg:.2f}')
+axs[0].axvline(median_avg, color='green', linestyle='--', label=f'Median: {median_avg:.2f}')
+axs[0].axvline(mode_avg, color='blue', linestyle='--', label=f'Mode: {mode_avg:.2f}')
 
-sns.histplot(data=df, x="attendance_rate", hue="Dropout Status", kde=True, ax=axs[1])
-axs[1].set_title("Density Plot Attendance Rate", fontsize=20)
+sns.histplot(data=df, x="attendance_rate", hue="Dropout Status", kde=True, ax=axs[1], stat='percent', common_norm=False)
+mean_att = df['attendance_rate'].mean()
+median_att = df['attendance_rate'].median()
+mode_att = df['attendance_rate'].mode()[0]
+
+axs[1].set_title("Distribution of Attendance Rate", fontsize=20)
 axs[1].set_xlabel("Attendance Rate", fontsize=20)
-axs[1].set_ylabel("Density", fontsize=20)
+axs[1].set_ylabel("", fontsize=20)
 axs[1].tick_params(axis='x', labelsize=15)
 axs[1].tick_params(axis='y', labelsize=15)
+axs[1].yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{x / 100:.2f}'))
+axs[1].axvline(mean_att, color='red', linestyle='--', label=f'Mean: {mean_att:.2f}')
+axs[1].axvline(median_att, color='green', linestyle='--', label=f'Median: {median_att:.2f}')
+axs[1].axvline(mode_att, color='blue', linestyle='--', label=f'Mode: {mode_att:.2f}')
 
-sns.histplot(data=df, x="passed_percent", hue="Dropout Status", kde=True, ax=axs[2])
-axs[2].set_title("Density Plot Passed Credit Percentage", fontsize=20)
-axs[2].set_xlabel("Passed Credit Percentage", fontsize=20)
-axs[2].set_ylabel("Density", fontsize=20)
+sns.histplot(data=df, x="passed_percent", hue="Dropout Status", kde=True, ax=axs[2], stat='percent', common_norm=False)
+mean_pass = df['passed_percent'].mean()
+median_pass = df['passed_percent'].median()
+mode_pass = df['passed_percent'].mode()[0]
+
+axs[2].set_title("Distribution of Passed Credit Rate", fontsize=20)
+axs[2].set_xlabel("Passed Credit Rate", fontsize=20)
+axs[2].set_ylabel("", fontsize=20)
 axs[2].tick_params(axis='x', labelsize=15)
 axs[2].tick_params(axis='y', labelsize=15)
+axs[2].yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{x / 100:.2f}'))
+axs[2].axvline(mean_pass, color='red', linestyle='--', label=f'Mean: {mean_pass:.2f}')
+axs[2].axvline(median_pass, color='green', linestyle='--', label=f'Median: {median_pass:.2f}')
+axs[2].axvline(mode_pass, color='blue', linestyle='--', label=f'Mode: {mode_pass:.2f}')
 
 # Save the figure
-output_path = 'Density Plot of Average Score, Attendance Rate and Passed Credit Percentage.png'
+output_path = 'Distribution of Average Score, Attendance Rate and Passed Credit Rate.png'
 fig.savefig(output_path, bbox_inches='tight')
 plt.show()
